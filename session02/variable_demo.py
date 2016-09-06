d18d0a7bd2c1c06a6b8ea4c69052ea8ede0ae939
Zhi Li <zli@babson.edu>
2016-09-06 15:24:01 -0400

    update session 02

---
+++ session02/variable_demo.py
@@ -0,0 +1,13 @@
+#print('Hello, world!')
+#print('Hey Jude', "don't make it bad")
+#print ('The total number of overall medals in Rio 2016 is', 46 + 37 + 38)
+
+
+
+a = 123 # a is an integer
+print(a)
+a = 'ABC' # a becomes a string
+print(a)
+
+
+input()
