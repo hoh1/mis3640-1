34a7e7577e18106f3e576e61fd61e395fb107a3f
Zhi Li <zli@babson.edu>
2016-09-15 14:41:14 -0400

    update mypolygon.py exercise 2.1 2.2 2.3

--- session05/mypolygon.py
+++ session05/mypolygon.py
@@ -1,10 +1,31 @@
 import turtle
+
+'''
+#Exercise 2.1
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+'''
+
+# Exercise 2.2
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+# Exercise 2.3
+def polygon(t, length, n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+
+
 jerry = turtle.Turtle()
-print(jerry)
+#square(jerry, 100) 
+polygon(jerry, 50, 12)
 
-jerry.fd(100)
-jerry.fd(100)
-jerry.lt(90)
-jerry.fd(100)
+alex = turtle.Turtle()
+square(alex, 30, 8) 
 
-turtle.mainloop()
+turtle.mainloop()  
