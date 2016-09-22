2beb20a0f38f1a71609a29621776ef9ad53750d6
Zhi Li <lizhiblack@gmail.com>
2016-09-21 20:46:00 -0400

    move to session 07

--- session04/quadratic_function.py
+++ session04/quadratic_function.py
@@ -1,24 +1,23 @@
 import math
 
+
 def quadratic(a, b, c):
-    A = b**2-4*a*c # calculate the discriminant
-    
-    if A >= 0: #equation has solutions
-        X1 = ((-b + math.sqrt(A))/2*a)
-        X2 = ((-b - math.sqrt(A))/2*a)
-        return X1,X2
-    
+    discriminant = b**2 - 4 * a * c  # calculate the discriminant
+
+    if discriminant >= 0:  # equation has solutions
+        x_1 = ((-b + math.sqrt(discriminant)) / 2 * a)
+        x_2 = ((-b - math.sqrt(discriminant)) / 2 * a)
+        return x_1, x_2
+
     else:
-        #print('No Real Number Solution')
-        return "Anything"
+        print('No Real Number Solution.')
+        return None
 
 
-#print(quadratic(2,2,2))
-#print(quadratic(1,4,1))
+# print(quadratic(2,2,2))
+# print(quadratic(1,4,1))
 
-a = int(input('please enter a number:'))
-b = int(input('please enter a number:'))
-c = int(input('please enter a number:'))
-print('results are:')
-msg = quadratic(a,b,c)
-print(msg)
+a = float(input('please enter a number:'))
+b = float(input('please enter a number:'))
+c = float(input('please enter a number:'))
+print('Results are:', quadratic(a, b, c))
