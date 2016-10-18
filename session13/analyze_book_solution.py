ee647ead1370ca5cf35dfd213328018a5870685f
Zhi Li <zli@babson.edu>
2016-10-18 15:23:19 -0400

    update analyze_book_solution

--- session13/analyze_book_solution.py
+++ session13/analyze_book_solution.py
@@ -11,7 +11,7 @@ def process_file(filename, skip_header):
     returns: map from each word to the number of times it appears.
     """
     hist = {}
-    fp = open(filename)
+    fp = open(filename, encoding='utf8')
 
     if skip_header:
         skip_gutenberg_header(fp)
@@ -19,8 +19,16 @@ def process_file(filename, skip_header):
     for line in fp:
         if line.startswith('*** END OF THIS PROJECT'):
             break
-        # INSERT CODE BELOW
-        pass
+        line = line.replace('-', ' ')
+        strippables = string.punctuation + string.whitespace
+
+        for word in line.split():
+            # remove punctuation and convert to lowercase
+            word = word.strip(strippables)
+            word = word.lower()
+
+            # update the histogram
+            hist[word] = hist.get(word, 0) + 1
 
     return hist
 
@@ -37,22 +45,28 @@ def skip_gutenberg_header(fp):
 
 def total_words(hist):
     """Returns the total of the frequencies in a histogram."""
-    pass
+    return sum(hist.values())
 
 
 def different_words(hist):
     """Returns the number of different words in a histogram."""
-    pass
+    return len(hist)
 
 
 def most_common(hist):
-    """Makes a list of word-freq pairs in descending order of frequency.
+    """Makes a list of word-freq pairs(tuples) in descending order of frequency.
 
     hist: map from word to frequency
 
     returns: list of (frequency, word) pairs
     """
-    pass
+    t = []
+    for key, value in hist.items():
+        t.append((value, key))
+
+    t.sort()
+    t.reverse()
+    return t
 
 
 def print_most_common(hist, num=10):
@@ -61,7 +75,10 @@ def print_most_common(hist, num=10):
     hist: histogram (map from word to frequency)
     num: number of words to print
     """
-    pass
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, '\t', freq)
 
 
 def subtract(d1, d2):
@@ -69,7 +86,11 @@ def subtract(d1, d2):
 
     d1, d2: dictionaries
     """
-    pass
+    res = {}
+    for key in d1:
+        if key not in d2:
+            res[key] = None
+    return res
 
 
 def random_word(hist):
@@ -77,7 +98,11 @@ def random_word(hist):
 
     The probability of each word is proportional to its frequency.
     """
-    pass
+    t = []
+    for word, freq in hist.items():
+        t.extend([word] * freq)
+
+    return random.choice(t)
 
 
 def main():
