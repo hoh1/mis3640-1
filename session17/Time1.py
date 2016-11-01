541102e1dbc3cf84e8e5243248de4a748124796a
Zhi Li <zli@babson.edu>
2016-11-01 14:47:49 -0400

    update

--- session17/Time1.py
+++ session17/Time1.py
@@ -5,6 +5,11 @@ class Time:
     attributes: hour, minute, second
     """
 
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
     def print_time(self):
         """Prints a string representation of the time."""
         print('%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second))
@@ -17,6 +22,9 @@ class Time:
     def is_after(self, other):
         return self.time_to_int() > other.time_to_int()
 
+    def is_as_expected(self, duration, arrival):
+        return self.time_to_int() + duration.time_to_int() == arrival.time_to_int()
+
     def increment(self, seconds):
         seconds += self.time_to_int()
         return int_to_time(seconds)
@@ -27,19 +35,34 @@ def int_to_time(seconds):
 
     seconds: int seconds since midnight.
     """
-    time = Time()
-    minutes, time.second = divmod(seconds, 60)
-    time.hour, time.minute = divmod(minutes, 60)
-    return time
+
+    minutes, second = divmod(seconds, 60)
+    hour, minute = divmod(minutes, 60)
+
+    return Time(hour, minute, second)
 
 
-start = Time()
-start.hour = 9
-start.minute = 45
-start.second = 0
+start = Time(9, 45, 0)
+# start.hour = 9
+# start.minute = 45
+# start.second = 0
 start.print_time()
 print(start.time_to_int())
 
 end = start.increment(2000)
 end.print_time()
 print(end.is_after(start))
+
+# traffic = Time()
+# traffic.hour = 0
+# traffic.minute = 30
+# traffic.second = 0
+
+# expected_time = Time()
+# expected_time.hour = 10
+# expected_time.minute = 15
+# expected_time.second = 0
+
+# traffic.print_time()
+# expected_time.print_time()
+# print(start.is_as_expected(traffic, expected_time))
