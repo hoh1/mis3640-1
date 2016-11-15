d51f29b45eb7c470bc8911422978fad85ac859fe
Zhi Li <zli@babson.edu>
2016-11-15 12:48:00 -0500

    add exam

---
+++ exam/exam_p1.py
@@ -0,0 +1,24 @@
+def speak_Chinese(number):
+    '''
+    number: a integer, 0<=number<99
+
+    Returns: a string that is the number in Chinese
+    '''
+    pass
+
+
+# For testing
+def main():
+    print(speak_Chinese('36'))
+    print('In Chinese: 36 = san shi liu')
+    print(speak_Chinese('20'))
+    print('In Chinese: 20 = er shi')
+    print(speak_Chinese('16'))
+    print('In Chinese: 16 = shi liu')
+    print(speak_Chinese('109'))
+    print('In Chinese: 109 = yi bai ling jiu')
+        print(speak_Chinese('999'))
+    print('In Chinese: 999 = jiu bai jiu shi jiu')
+
+if __name__ = '__main__':
+    main()
